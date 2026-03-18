7930380fcbd9d94821b008762663ba7472293725
RahJess <jessicakantotiana@gmail.com>
2026-03-18 18:18:12 +0100

    add: condition for header for supporting the format text/plain

--- src/main/java/org/example/td2introspring/controller/StudentController.java
+++ src/main/java/org/example/td2introspring/controller/StudentController.java
@@ -1,23 +1,33 @@
 package org.example.td2introspring.controller;
 
 import org.example.td2introspring.entity.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
-    private List<Student> students = new ArrayList<>();
+    private List<Student> studentsList = new ArrayList<>();
 
     @PostMapping("/students")
     public Student createStudent(@RequestBody Student student) {
-        students.add(student);
+        studentsList.add(student);
         return student;
     }
 
     @GetMapping("/students")
-    public List<Student> getAllStudents() {
-        return students;
+    public ResponseEntity<String> getStudentNames(@RequestHeader(value = "Accept", defaultValue = "text/plain") String acceptHeader) {
+        if(!acceptHeader.equals("text/plain")) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Format non supporté");
+        }
+
+        String names = studentsList.stream()
+                .map(Student::getFirstName)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.ok(names);
     }
 }
