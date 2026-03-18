9a52f7d30bc73392faf4e518b1ca6572b57fe42e
RahJess <jessicakantotiana@gmail.com>
2026-03-18 18:10:43 +0100

    add: endpoint to get all students and update post endpoint to stock new student in an array

--- src/main/java/org/example/td2introspring/controller/StudentController.java
+++ src/main/java/org/example/td2introspring/controller/StudentController.java
@@ -1,14 +1,23 @@
 package org.example.td2introspring.controller;
 
 import org.example.td2introspring.entity.Student;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
+    private List<Student> students = new ArrayList<>();
+
     @PostMapping("/students")
     public Student createStudent(@RequestBody Student student) {
+        students.add(student);
         return student;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return students;
     }
 }
