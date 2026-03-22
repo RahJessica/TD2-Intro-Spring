32d2a93dcc2b4d1435e037db93055207c864cf7d
RahJess <jessicakantotiana@gmail.com>
2026-03-22 15:11:55 +0100

    add: personalized http response for endpoint /welcome

--- src/main/java/org/example/td2introspring/controller/WelcomingController.java
+++ src/main/java/org/example/td2introspring/controller/WelcomingController.java
@@ -1,14 +1,19 @@
     package org.example.td2introspring.controller;
 
     import org.example.td2introspring.entity.Welcoming;
+    import org.springframework.http.ResponseEntity;
     import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.web.bind.annotation.RequestParam;
     import org.springframework.web.bind.annotation.RestController;
 
     @RestController
     public class WelcomingController {
+        // required false -> gestion de l'absence du paramètre par moi-même != required true où le param est exigé et son absence est directement géré par spring
         @GetMapping("/welcome")
-        public Welcoming welcome(@RequestParam String name) {
-            return new Welcoming(name);
+        public ResponseEntity<?> welcome(@RequestParam(required = false) String name) {
+            if (name == null || name.isEmpty()) {
+                return ResponseEntity.status(400).body("Paramètre name manquant");
+            }
+            return ResponseEntity.ok(new Welcoming(name));
         }
     }
