ca2207c989af3f9a9c1ab1f05ef326b5730350ac
RahJess <jessicakantotiana@gmail.com>
2026-03-25 20:10:29 +0100

    feat: implement validation for nullable params in StudentValidator

--- src/main/java/org/example/td2introspring/validator/StudentValidator.java
+++ src/main/java/org/example/td2introspring/validator/StudentValidator.java
@@ -1,4 +1,12 @@
 package org.example.td2introspring.validator;
 
+import org.example.td2introspring.dto.StudentRequest;
+
 public class StudentValidator {
+    public void validate(StudentRequest request) {
+
+        if (request.getReference() == null || request.getReference().isBlank()) {
+            throw new IllegalArgumentException("Reference obligatoire");
+        }
+    }
 }
