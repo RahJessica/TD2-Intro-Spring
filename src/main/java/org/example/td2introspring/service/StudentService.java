620d06b2aa4681f6092e856f14bb5555f70f0aa3
RahJess <jessicakantotiana@gmail.com>
2026-03-25 20:12:47 +0100

    feat: implement logic and call validator in service responsability

--- src/main/java/org/example/td2introspring/service/StudentService.java
+++ src/main/java/org/example/td2introspring/service/StudentService.java
@@ -1,4 +1,31 @@
 package org.example.td2introspring.service;
 
+import org.example.td2introspring.dto.StudentRequest;
+import org.example.td2introspring.entity.Student;
+import org.example.td2introspring.validator.StudentValidator;
+import java.util.ArrayList;
+import java.util.List;
+
 public class StudentService {
+    private final StudentValidator validator;
+    private final List<Student> students = new ArrayList<>();
+
+    public StudentService(StudentValidator validator) {
+        this.validator = validator;
+    }
+
+    public List<Student> createStudents(List<StudentRequest> requests) {
+
+        for (StudentRequest req : requests) {
+
+            validator.validate(req);
+
+            Student s = new Student();
+            s.setReference(req.getReference());
+
+            students.add(s);
+        }
+
+        return students;
+    }
 }
